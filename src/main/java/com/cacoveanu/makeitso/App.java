ee4b089fdf517b286d790dc6f72cf99534c2b99e
Silviu Cacoveanu <scacoveanu@ingenuity.com>
2015-07-02 11:32:04 +0300

    initialising repo

---
+++ src/main/java/com/cacoveanu/makeitso/App.java
@@ -0,0 +1,13 @@
+package com.cacoveanu.makeitso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
